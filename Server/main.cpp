50edce93875ec842baae27269d1ef4f46c6960bb
DariusJderu1 <jderudarius@gmail.com>
2026-03-02 13:59:36 +0200

    Add threading functionality

--- Server/main.cpp
+++ Server/main.cpp
@@ -4,7 +4,26 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include<thread>
 using namespace std;
+void executes_server_client_connection(int fd_new_server, struct sockaddr_in client_info)
+{
+    // Receive the message from the client
+    char client_message[256] = {0};
+    recv(fd_new_server, client_message, sizeof(client_message), 0);
+
+    // Print a message with the client's message and ports
+    string server_message = string(client_message) + " | Client IP: " +
+        inet_ntoa(client_info.sin_addr) + " | Client port: " + to_string(ntohs(client_info.sin_port)) +
+            " | Server IP: " + "127.0.0.1" + " | Server port: " + to_string(8080);
+    cout << "Message from client: " << server_message << "\n";
+
+    // send the message back to the client
+    send(fd_new_server, server_message.c_str(), server_message.size(), 0);
+
+    //close the server socket
+    close(fd_new_server);
+}
 int main()
 {
     // create the server socket - a file descriptor
@@ -38,34 +57,20 @@ int main()
     while(true)
     {
         // accepting requests from clients (and keeping their info)
-        // fd_new_server is the file descriptor assigned for the current client
+        // fd_new_server is the socket (file descriptor) assigned
+        // for the communication with the current client
         struct sockaddr_in client_info;
         socklen_t client_info_size = sizeof(struct sockaddr_in);
         int fd_new_server = accept(server_socket, (struct sockaddr*)&client_info, &client_info_size);
         if(fd_new_server < 0)
-        {
             cout << "Connection to client failed";
-            return 0;
-        }
         else
             cout << "Client connected\n";
 
 
-        // Receive the message from the client
-        char client_message[256] = {0};
-        recv(fd_new_server, client_message, sizeof(client_message), 0);
+        thread t(executes_server_client_connection, fd_new_server, client_info);
 
-        // Print a message with the client's message and ports
-        string server_message = string(client_message) + " | Client IP: " +
-            inet_ntoa(client_info.sin_addr) + " | Client port: " + to_string(ntohs(client_info.sin_port)) +
-                " | Server IP: " + "127.0.0.1" + " | Server port: " + to_string(8080);
-        cout << "Message from client: " << server_message << "\n";
-
-        // send the message back to the client
-        send(fd_new_server, server_message.c_str(), server_message.size(), 0);
-
-        //close the server socket
-        close(fd_new_server);
+        t.detach();
     }
 
     return 0;
